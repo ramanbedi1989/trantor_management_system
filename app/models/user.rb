c86cc1da03b700c122aa5f10abda7cbfc0af3919
Prashant Sahni <prashant.sahni5@gmail.com>
2015-02-25 19:52:39 +0530

    carry forward casual leaves

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,23 @@ class User < ActiveRecord::Base
     projects.map(&:name).join(';')
   end
 
+#------------------------------------------------------------------------------------
+
+  def casual_leaves
+    self.leave_credits.where("leave_id =?", Leave.casual_leave.id)
+  end
+
+  def sick_leaves
+    self.leave_credits.where("leave_id =?", Leave.sick_leave.id)
+  end
+
+  def earned_leaves
+    self.leave_credits.where("leave_id =?", Leave.earned_leave.id)
+  end
+
+#------------------------------------------------------------------------------------
+
+
   # INCREMENT UPTO 36
   MAX_EARNED_LEAVES = 36
   def increment_earned_leaves(increment_by=1)
@@ -55,6 +72,59 @@ class User < ActiveRecord::Base
     left_leaves = MAX_EARNED_LEAVES - earned_leaves
     if increment_by <= left_leaves
       IncrementLeaves.increment_earned_leaves([self], increment_by)
+    end
+  end
+
+#------------------------------------------------------------------------------------
+
+  def carry_forward_casual_leaves
+    casual_leaves_count = self.leave_credits.where("leave_id =?", Leave.casual_leave.id).count
+    sick_leaves_count = self.leave_credits.where("leave_id =?", Leave.sick_leave.id).count       
+    if casual_leaves_count > 4
+      delete_count = casual_leaves_count - 4     #Delete all casual leaves leaving 4
+      destroy_casual_leaves(delete_count)
+      delete_count = nil
+      destroy_sick_leaves(delete_count)         # Delete all sicak leaves
+      return
+    end 
+    case casual_leaves_count
+      when 4                                      #Destroy all sick leaves
+        delete_count = nil
+      when 3                                      #Destroy all sick leaves leaving 1        
+        delete_count = sick_leaves_count - 1
+      when 2                                      #Destroy all sick leaves leaving 2
+        delete_count = sick_leaves_count - 2
+      when 1                                      #Destroy all sick leaves leaving 3
+        delete_count = sick_leaves_count - 3
+      when 0                                      #Destroy all sick leaves leaving 4
+        delete_count = sick_leaves_count - 4
+      end
+    destroy_sick_leaves(delete_count)
+  end
+
+#------------------------------------------------------------------------------------
+
+
+  def destroy_sick_leaves(delete_count)
+    sick_leaves = self.leave_credits.where("leave_id =?", Leave.sick_leave.id).order('created_at')
+    if delete_count == nil      
+      sick_leaves.each do |s|
+        s.destroy
+      end
+      return
+    end
+    return if delete_count < 1
+    delete_count.times do |i|
+      sick_leaves[i].destroy
+    end
+  end
+
+#------------------------------------------------------------------------------------
+
+  def destroy_casual_leaves(delete_count)
+    casual_leaves = self.leave_credits.where("leave_id =?", Leave.casual_leave.id)
+     delete_count.times do |i|
+      casual_leaves[i].destroy
     end
   end
 
