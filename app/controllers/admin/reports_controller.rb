809acbddb9b1962c856aa8125bbdf233545c91c3
Prashant Sahni <prashant.sahni5@gmail.com>
2015-05-25 17:27:34 +0530

    code for attendance report generation

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,12 @@
 class Admin::ReportsController < ApplicationController
 
+  def index
+  end
+
+  def attendance
+  end
+
+
 	def loss_of_pays
 		@loss_of_pays = LossOfPay.includes(:loss_of_pay_infos)
   end
@@ -14,6 +21,9 @@ class Admin::ReportsController < ApplicationController
         when :lop
           file = @report.generate_lop_report
           send_file(file)    
+       when :attendance
+        file = @report.generate_attendance_report
+        send_file(file)    
       end
       rescue => e
         log_error(e)
