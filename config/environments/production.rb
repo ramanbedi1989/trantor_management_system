cbbc074b7264a15dc18095564ba369fb49be89fd
Moin Haidar <moinhaidar@gmail.com>
2015-09-07 15:38:06 +0530

    Update configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,19 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  
+  config.assets.enabled = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
