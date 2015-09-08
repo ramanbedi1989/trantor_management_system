45c90a4c4794e619c8a771ba0689bcba83d6f863
Moin Haidar <moinhaidar@gmail.com>
2015-09-08 15:20:28 +0530

    Load remote db yml based on env

--- config/initializers/load_attendance_db.rb
+++ config/initializers/load_attendance_db.rb
@@ -1,1 +1,3 @@
-DB_ATTENDANCE = YAML.load_file("#{Rails.root}/config/attendance.yml")['attendance']
+DB_ATTENDANCE = Rails.env == 'production' ? 
+	YAML.load_file("#{Rails.root}/config/attendance.yml")['attendance'] :
+	YAML.load_file("#{Rails.root}/config/remote_db.yml")['attendance']
