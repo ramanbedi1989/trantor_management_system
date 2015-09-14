1bbc03605bc8241dfb3bb822560cd9bba94d8fba
Moin Haidar <moinhaidar@gmail.com>
2015-09-14 14:09:00 +0530

    Remove attendance credentials

--- config/initializers/load_attendance_db.rb
+++ config/initializers/load_attendance_db.rb
@@ -1,3 +1,1 @@
-DB_ATTENDANCE = Rails.env == 'production' ? 
-	YAML.load_file("#{Rails.root}/config/attendance.yml")['attendance'] :
-	YAML.load_file("#{Rails.root}/config/remote_db.yml")['attendance']
+DB_ATTENDANCE = YAML.load_file("#{Rails.root}/config/attendance.yml")['attendance']
